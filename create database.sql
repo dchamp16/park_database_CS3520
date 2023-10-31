138d98c4264211fbbd7837667384179983aa1a4b
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2023-10-31 20:21:01 +0000

    added some comments

--- create database.sql
+++ create database.sql
@@ -17,16 +17,16 @@ CREATE TABLE IF NOT EXISTS Wildlife (
     name VARCHAR(20) NOT NULL,
     species VARCHAR(10),
     description VARCHAR(500),
-    threatenedStatus VARCHAR(6), -- HIGH, MEDIUM, LOW
+    threatenedStatus VARCHAR(6), -- HIGH, MEDIUM, LOW -- Peter: ENUM
     population VARCHAR(15) -- 10 million, 0.1 million???????????
 );
 
 CREATE TABLE IF NOT EXISTS EmergencyIncident (
 	incidentID INT PRIMARY KEY AUTO_INCREMENT,
-    date DATETIME NOT NULL, -- Time is necessary?????????
-    type CHAR(2), -- what's the type????????????
-    location VARCHAR(100) NOT NULL,
-    status VARCHAR(10) NOT NULL, -- Reported, Responding, Active, Resolved, Closed
+    date DATETIME NOT NULL, -- Time is necessary????????? -- Peter: necessary because they need a record for when the incident occure
+    type CHAR(2), -- what's the type???????????? -- Peter: we dont need this too we can address this to the description
+    location VARCHAR(100) NOT NULL, -- Peter: we dont need this too we can address this to the description
+    status VARCHAR(10) NOT NULL, -- Reported, Responding, Active, Resolved, Closed -- Peter: im htink we can do 0,1 for this and create a 2 table if 1 for resolve and 0 for not resolve? maybe
     description VARCHAR(500),
     parkID SMALLINT,
     FOREIGN KEY (parkID) REFERENCES Park(parkID)
@@ -45,8 +45,8 @@ CREATE TABLE IF NOT EXISTS Event (
 CREATE TABLE IF NOT EXISTS Trail (
 	trailID SMALLINT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(50) NOT NULL,
-	difficulty VARCHAR(6), -- HIGH, MEDIUM, LOW
-    length FLOAT, -- what's the length??????????
+	difficulty VARCHAR(6), -- HIGH, MEDIUM, LOW -- Peter: ENUM
+    length FLOAT, -- what's the length?????????? -- Peter:  length of how long people will finish the trail, this should be not float? it should be varchar usually they put output:30min or etc. but in my opinion we can remove this and add it to description
     description VARCHAR(500),
     parkID SMALLINT,
     FOREIGN KEY (parkID) REFERENCES Park(parkID)
@@ -56,10 +56,10 @@ CREATE TABLE IF NOT EXISTS Visitor (
 	visitorID INT PRIMARY KEY AUTO_INCREMENT,
     firstName VARCHAR(20) NOT NULL,
     lastName VARCHAR(20),
-    DOB DATE, -- date of birth
+    DOB DATE, -- date of birth -- Peter: this look ok unless if you guys want this VARCHAR?
     email VARCHAR(30) NOT NULL,
-    COUNTRY CHAR(2), -- US, CN, JP
-    phone VARCHAR(15) NOT NULL -- different countries
+    COUNTRY CHAR(2), -- US, CN, JP -- Peter: True. this is fine
+    phone VARCHAR(15) NOT NULL -- different countries -- Peter: dont worry about it if its a from a different country i dont think prof will check it
 );
 
 CREATE TABLE IF NOT EXISTS ParkVisitor (
@@ -91,7 +91,7 @@ CREATE TABLE IF NOT EXISTS Staff (
 CREATE TABLE IF NOT EXISTS Facility (
 	facilityID SMALLINT PRIMARY KEY AUTO_INCREMENT,
     capacity SMALLINT UNSIGNED NOT NULL,
-    type CHAR(2), -- what's the type????????????
+    type CHAR(2), -- what's the type???????????? -- Peter: we discussed this in discord but if we meet the 10 tables we can just getrid of some of attributes and put everything in description
     description VARCHAR(500),
     location VARCHAR(100) NOT NULL,
     parkID SMALLINT,
